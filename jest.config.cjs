6de3f608424be6d7496e91746dcf57e1a942a9db
Yasmineibr1 <yasmine.brahimi321@gmail.com>
2025-08-21 00:25:08 +0200

    fix:logger import 2

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,8 @@
+const path = require('path'); 
+
 /** @type {import('jest').Config} */
-
-
 module.exports = {
-  rootDir: path.resolve(__dirname),
+  rootDir: path.resolve(), 
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.js"],
   clearMocks: true,
@@ -12,8 +12,7 @@ module.exports = {
     "!src/**/index.js"
   ],
   moduleNameMapper: {
-    '^@root/(.*)$': '<rootDir>/$1', 
+    '^@root/(.*)$': '<rootDir>/$1',
   },
-
-  moduleDirectories: ['node_modules', '.'], 
+  moduleDirectories: ['node_modules', '.'],
 };
